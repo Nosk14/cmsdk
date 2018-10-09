ffd4f65108170a7815cbfd4daea17eae14b2952c
Carlos Lucas Acosta <nosk14@gmail.com>
2018-10-09 18:07:23 +0200

    basic setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from cmsdk import __version__
+
+setup(name='cmsdk',
+      version=__version__,
+      description='Card Market SDK',
+      author='Carlos Lucas',
+      author_email='nosk14@gmail.com',
+      url='https://github.com/Nosk14/cmsdk',
+      packages=['cmsdk'],
+     )
