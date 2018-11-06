3d716a0bb392ffae2b3ea2bf2e706dd54348590e
Carlos Lucas <nosk14@gmail.com>
2018-11-06 20:35:09 +0100

    Added method to build entities from json and added missing fields to Account

--- cmsdk/entities.py
+++ cmsdk/entities.py
@@ -1,8 +1,15 @@
 from dataclasses import dataclass
 
 
+class BaseEntity:
+
+    @classmethod
+    def from_json(cls, json):
+        return cls(**json)
+
+
 @dataclass
-class Address:
+class Address(BaseEntity):
     name: str
     extra: str
     street: str
@@ -12,17 +19,19 @@ class Address:
 
 
 @dataclass
-class Name:
+class Name(BaseEntity):
     firstName: str
     lastName: str
 
+
 @dataclass
-class MoneyDetails:
+class MoneyDetails(BaseEntity):
     totalBalance: float
     moneyBalance: float
     bonusBalance: float
     unpaidAmount: float
     providerRechargeAmount: float
+
 
 @dataclass
 class BankAccount:
@@ -31,29 +40,47 @@ class BankAccount:
     bic: str
     bankName: str
 
+    @classmethod
+    def from_json(cls, json):
+        return BankAccount(**json)
+
+
 @dataclass
-class Account:
+class Account(BaseEntity):
     idUser: int
     username: str
     country: str
     isCommercial: int
     maySell: int
+    sellerActivation: int
     riskGroup: int
+    lossPercentage: str
     reputation: int
     shipsFast: int
     sellCount: int
-    soldIntems: int
+    soldItems: int
     avgShippingTime: int
     onVacation: bool
-    idDisplaylanguage: int
+    idDisplayLanguage: int
     name: Name
     homeAddress: Address
     email: str
     phoneNumber: str
     vat: str
+    legalInformation: str
     registerDate: str
+    isActivated: bool
     moneyDetails: MoneyDetails
     bankAccount: BankAccount
     articlesInShoppingCart: int
     unreadMessages: int
-    links: dict
+
+    @classmethod
+    def from_json(cls, json):
+        sub_entities = {
+            'name' : Name.from_json(json['name']),
+            'homeAddress' : Address.from_json(json['homeAddress']),
+            'moneyDetails' : MoneyDetails.from_json(json['moneyDetails']),
+            'bankAccount' : BankAccount.from_json(json['bankAccount'])
+        }
+        return Account(**{**json, **sub_entities})
